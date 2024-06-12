7bdc17cb1f41c7d06cb92f06214fe7fe64ef940c
ibrahim11elian <ibrahim11elian@gmail.com>
2024-06-13 02:24:53 +0300

    add cart routes

---
+++ src/routes/cart-routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import Cart from "../controllers/cart";
+import Authentication from "../controllers/authentication";
+
+const router = Router({
+  mergeParams: true,
+});
+const auth = new Authentication();
+const cart = new Cart();
+
+router.route("/").all(auth.protect).post(cart.addOrUpdateCartItem).get(cart.getCartList);
+router.route("/:id").all(auth.protect, cart.checkCartOwner).patch(cart.updateCartItem).delete(cart.deleteCartItem);
+
+export default router;
