ba21d9d6676d74257a956f1fbdba0453ff37977c
ibrahim11elian <ibrahim11elian@gmail.com>
2024-06-13 02:15:48 +0300

    implemented the product controller

---
+++ src/controllers/product.ts
@@ -0,0 +1,16 @@
+import Product, { IProduct } from "../models/product/product";
+import BaseController from "./base";
+
+class ProductController extends BaseController<IProduct> {
+  constructor() {
+    super(Product);
+  }
+
+  createProduct = this.createOne;
+  getProducts = this.getAll;
+  getProduct = this.getOne();
+  updateProduct = this.updateOne;
+  deleteProduct = this.deleteOne;
+}
+
+export default ProductController;
