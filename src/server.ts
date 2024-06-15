467d27604505f08113235d5f07d88574c70cc7be
ibrahim11elian <ibrahim11elian@gmail.com>
2024-06-15 21:00:28 +0300

    move the DB connection after server creation so if there is any failing in connection stop the server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 import app from "./app";
 
+const port = process.env.PORT || 3000;
+// Starting The Server
+const server = app.listen(port, () => {
+  console.log(`${process.env.NODE_ENV} server is running on port ${port}`);
+});
+
 // Connecting to the Database
 mongoose
   .connect(process.env.DB_HOST as string)
@@ -9,13 +15,8 @@ mongoose
   })
   .catch(() => {
     console.log("Failed to connect to the Database");
+    shutDownServer();
   });
-
-const port = process.env.PORT || 3000;
-// Starting The Server
-const server = app.listen(port, () => {
-  console.log(`${process.env.NODE_ENV} server is running on port ${port}`);
-});
 
 // this will handle any rejection in the entire application
 process.on("unhandledRejection", shutDownServer);
