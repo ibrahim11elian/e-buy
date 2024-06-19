f87723bcd004a1550872f2e46c658b1f652dcd09
ibrahim11elian <ibrahim11elian@gmail.com>
2024-06-20 00:57:24 +0300

    add more error handling functions for different scenarios

--- src/controllers/error.ts
+++ src/controllers/error.ts
@@ -1,11 +1,31 @@
 /* eslint-disable no-unused-vars */
 import { Request, Response, NextFunction } from "express";
 import AppError from "../utils/error";
+import { MongooseError } from "mongoose";
 
 /** Global error handling middleware for sending the error in both development and production */
-export default function (err: AppError | Error, req: Request, res: Response, next: NextFunction) {
+export default function (
+  err: AppError | Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   if (process.env.NODE_ENV === "production") {
-    sendErrorProd(err, res);
+    // Create a new error object, preserving the prototype chain
+    // this is used as spread operator as well as Object.assign did not work for prototype chain
+    let error: AppError | MongooseError | Error = Object.create(
+      Object.getPrototypeOf(err),
+      Object.getOwnPropertyDescriptors(err),
+    );
+
+    if (error.name === "CastError") error = handleCastError(error);
+    if (error.name === "ValidationError") error = handleValidationError(error);
+    // this error is specific to mongo not to mongoose so i need to extract the code of duplicate error from the massage
+    if (error.message.match(/E11000/g)) error = handleDuplicateFieldsDB(error);
+    if (error.name === "JsonWebTokenError") error = handleJWTError();
+    if (error.name === "TokenExpiredError") error = handleTokenExpiredError();
+
+    sendErrorProd(error, res);
   } else {
     sendErrorDev(err, res);
   }
@@ -41,4 +61,42 @@ function sendErrorDev(err: AppError | Error, res: Response) {
     error: err.message,
     stack: err.stack,
   });
+}
+
+function handleCastError(error: any): AppError {
+  const message = `Invalid ${error.path}: ${error.value}`;
+  return new AppError(message, 400);
+}
+
+function handleValidationError(error: any): AppError {
+  return new AppError(error.message, 400);
+}
+
+function handleDuplicateFieldsDB(err: any): AppError {
+  // Extract the duplicate keys and values from the error message
+  const matches = err.message.match(/dup key: {([^}]+)}/);
+  if (!matches) {
+    return new AppError(
+      "Duplicate field value(s). Please use another value(s)!",
+      400,
+    );
+  }
+  const fields = matches[1].split(",").map((field: string) => field.trim());
+
+  // Construct a dynamic error message
+  const fieldMessages = fields.map((field: string) => {
+    const [key, value] = field.split(":").map((part) => part.trim());
+    return `${key}: ${value}`;
+  });
+
+  const message = `Duplicate field value(s): ${fieldMessages.join(", ")}. Please use another value(s)!`;
+  return new AppError(message, 400);
+}
+
+function handleJWTError(): AppError {
+  return new AppError("Invalid token.", 401);
+}
+
+function handleTokenExpiredError(): AppError {
+  return new AppError("Token has expired.", 401);
 }
