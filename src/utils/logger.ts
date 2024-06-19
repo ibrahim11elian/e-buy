eca2abc61476d235a17da11757fa269e1304fece
ibrahim11elian <ibrahim11elian@gmail.com>
2024-06-20 01:01:23 +0300

    add logger handler by winston for logging

---
+++ src/utils/logger.ts
@@ -0,0 +1,15 @@
+import { createLogger, format, transports } from "winston";
+
+const logger = createLogger({
+  level: "info",
+  format: format.combine(
+    format.timestamp(),
+    format.json(), // Output logs in JSON format
+  ),
+  transports: [
+    new transports.File({ filename: "app.log" }),
+    // new transports.Console(),
+  ],
+});
+
+export default logger;
