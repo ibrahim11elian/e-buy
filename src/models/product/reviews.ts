fd0786d39f4e91d9615b42415984e2e946e89f0b
ibrahim11elian <ibrahim11elian@gmail.com>
2024-06-20 08:51:32 +0300

    update the review model with the functionality that calculate the number of product ratings and the ratings average

--- src/models/product/reviews.ts
+++ src/models/product/reviews.ts
@@ -1,11 +1,19 @@
-import mongoose, { Model, Schema } from "mongoose";
+/* eslint-disable no-unused-vars */
+import mongoose, { Model, Schema, Document, Query } from "mongoose";
+import Product from "./product";
 
-interface IReview {
+export interface IReview extends Document {
   rating: number;
   review: string;
   user: mongoose.Types.ObjectId;
   product: mongoose.Types.ObjectId;
   populate: () => Promise<IReview>;
+}
+
+interface IReviewDocument extends IReview {}
+
+export interface IReviewModel extends Model<IReview> {
+  calcAverageRatings(productId: mongoose.Types.ObjectId): Promise<void>;
 }
 
 const reviewsSchema: Schema<IReview> = new mongoose.Schema(
@@ -22,6 +30,8 @@ const reviewsSchema: Schema<IReview> = new mongoose.Schema(
     },
     rating: {
       type: Number,
+      min: [1, "Rating must be at least 1"],
+      max: [5, "Rating must be at most 5"],
       required: [true, "Rating is required"],
     },
     review: {
@@ -45,6 +55,54 @@ reviewsSchema.pre(/^find/, function (next) {
   next();
 });
 
-const Review: Model<IReview> = mongoose.model<IReview>("Review", reviewsSchema);
+reviewsSchema.statics.calcAverageRatings = async function (
+  productId: mongoose.Types.ObjectId,
+) {
+  const stats = await this.aggregate([
+    {
+      $match: { product: productId },
+    },
+    {
+      $group: {
+        _id: "$product",
+        nRating: { $sum: 1 },
+        avgRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+
+  await Product.findByIdAndUpdate(productId, {
+    numOfReviews: stats.length > 0 ? stats[0].nRating : 0,
+    ratingsAverage: stats.length > 0 ? stats[0].avgRating : 0,
+  });
+};
+
+reviewsSchema.post("save", async function () {
+  await (this.constructor as IReviewModel).calcAverageRatings(this.product);
+});
+
+reviewsSchema.pre<any>(/^findOneAnd/, async function (next) {
+  // pass the data from pre to post middleware
+  // reference the model to fetch the document before the update operation.
+  const doc = await (this.model as IReviewModel).findOne(this.getQuery());
+
+  if (doc) {
+    (this as any).r = doc;
+  }
+  next();
+});
+
+reviewsSchema.post<any>(/^findOneAnd/, async function (doc) {
+  if ((this as any).r) {
+    await (this as any).r.constructor.calcAverageRatings(
+      (this as any).r.product,
+    );
+  }
+});
+
+const Review: IReviewModel = mongoose.model<IReview, IReviewModel>(
+  "Review",
+  reviewsSchema,
+);
 
 export default Review;
