8afdc207594fcbd72831dc078a0fbd4e38b1a0aa
ibrahim11elian <ibrahim11elian@gmail.com>
2024-06-20 22:25:04 +0300

    add the profile controller functions, just update and get functions

---
+++ src/controllers/profile.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import Profile, { IProfile } from "../models/user/profile";
+import BaseController from "./base";
+
+class ProfileController extends BaseController<IProfile> {
+  constructor() {
+    super(Profile);
+  }
+
+  updateProfile = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      req.params.id = req.user.id;
+      return await this.updateOne(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  getProfile = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      req.params.id = req.user.id;
+      return await this.getOne()(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default ProfileController;
