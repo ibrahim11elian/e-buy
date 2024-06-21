e3a7143d26bb2b22dc411fc44c80a7a1439d881a
ibrahim11elian <ibrahim11elian@gmail.com>
2024-06-21 04:55:12 +0300

    add address controller with some functionality like create or update the address and get and delete address (for current user)

---
+++ src/controllers/address.ts
@@ -0,0 +1,82 @@
+import { NextFunction, Request, Response } from "express";
+import Address, { IAddress } from "../models/user/address";
+import BaseController from "./base";
+import mongoose from "mongoose";
+import User from "../models/user/user";
+
+class AddressController extends BaseController<IAddress> {
+  constructor() {
+    super(Address);
+  }
+
+  createAddress = async (req: Request, res: Response, next: NextFunction) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+      const address = await Address.findById(req.user.id).session(session);
+
+      if (address) {
+        req.params.id = req.user.id;
+        return await this.updateOne(req, res, next, session);
+      } else {
+        req.body._id = req.user.id;
+        const newAddress = new Address(req.body);
+        await newAddress.save({ session });
+
+        await User.findByIdAndUpdate(
+          req.user.id,
+          { address: req.user.id },
+          { session },
+        );
+
+        res.status(201).json({
+          status: "success",
+          data: newAddress,
+        });
+      }
+
+      await session.commitTransaction();
+      session.endSession();
+    } catch (error) {
+      await session.abortTransaction();
+      session.endSession();
+      next(error);
+    }
+  };
+
+  getAddress = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      req.params.id = req.user.id;
+      return await this.getOne()(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  deleteAddress = async (req: Request, res: Response, next: NextFunction) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+      req.params.id = req.user.id;
+
+      // Update the user document within the transaction
+      await User.findByIdAndUpdate(req.user.id, { address: null }).session(
+        session,
+      );
+
+      // Delete the address within the transaction
+      await this.deleteOne(req, res, next, session);
+
+      await session.commitTransaction();
+      session.endSession();
+    } catch (error) {
+      await session.abortTransaction();
+      session.endSession();
+      next(error);
+    }
+  };
+}
+
+export default AddressController;
